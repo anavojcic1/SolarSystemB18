7954cd022d941992f048a6aea7b86afb9c5c8282
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-17 12:26:28 -0500

    i have worked on Planet1

---
+++ src/main/java/FeatureSun.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class FeatureSun {
+    String color;
+    String size;
+    String temp;
+}
--- src/main/java/SolarSystem.java
+++ src/main/java/SolarSystem.java
@@ -4,5 +4,15 @@ public class SolarSystem {
         solar.Stars = "100";
         solar.Moon = "4";
         solar.Planets = "2";
+
+        FeatureSun xros = new FeatureSun();
+        xros.color="blue";
+        xros.temp="123456k";
+        xros.size="3km";
+
+        Planet1 frog = new Planet1();
+        frog.color = "blue";
+        frog.temp="345678k";
+        frog.size="2km";
     }
 }
