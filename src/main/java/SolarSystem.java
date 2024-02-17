ff235ffc351679da151fca10244c4e5797e28070
anavojcic1 <ana.vojcic92@gmail.com>
2024-02-17 11:14:59 -0500

    i have worked on featuresun please review and merge it

--- src/main/java/SolarSystem.java
+++ src/main/java/SolarSystem.java
@@ -4,5 +4,10 @@ public class SolarSystem {
         solar.Stars = "100";
         solar.Moon = "4";
         solar.Planets = "2";
+
+        FeatureSun xros = new FeatureSun();
+        xros.Color = "pink";
+        xros.temp = "3456787654345k";
+        xros.Size = "3456734miles";
     }
 }
